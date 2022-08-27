a1c87b0afb5a72220b1059a56610a994725aa11c
Ramon Olmos <ramon.olmos90@gmail.com>
2022-08-27 11:18:58 -0500

    modifico para generar link de pago

--- src/application/controllers/checkout.php
+++ src/application/controllers/checkout.php
@@ -6,13 +6,26 @@ class Checkout extends CI_Controller {
     }
     public function generar_link_pago(){
         $id_venta = $this->input->post('id_venta');
-        $monto = $this->input->post('monto');
-        $nombre_cliente = $this->input->post('nombre_cliente');
-        $ci_cliente = $this->input->post('ci_cliente');
-        $telefono_cliente = $this->input->post('telefono_cliente');
-        $direccion = $this->input->post('direccion');
-        $ciudad = $this->input->post('ciudad');
-        $sandbox = $this->input->post('sandbox');
+
+        if($id_venta) {
+            $monto = $this->input->post('monto');
+            $nombre_cliente = $this->input->post('nombre_cliente');
+            $ci_cliente = $this->input->post('ci_cliente');
+            $telefono_cliente = $this->input->post('telefono_cliente');
+            $direccion = $this->input->post('direccion');
+            $ciudad = $this->input->post('ciudad');
+            $sandbox = $this->input->post('sandbox');
+        } else {
+            $data = json_decode(file_get_contents('php://input'), true);
+            $id_venta = $data['id_venta'];
+            $monto = $data['monto'];
+            $nombre_cliente = $data['nombre_cliente'];
+            $ci_cliente = $data['ci_cliente'];
+            $telefono_cliente = $data['telefono_cliente'];
+            $direccion = $data['direccion'];
+            $ciudad = $data['ciudad'];
+            $sandbox = $data['sandbox'];
+        }
 
         $this->load->model('m_checkout');
         $id_pago = $this->m_checkout->generar($id_venta,$monto,$nombre_cliente,$ci_cliente,$telefono_cliente,$direccion,$ciudad,$sandbox);
